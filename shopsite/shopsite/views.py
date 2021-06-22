4005232ca0a8b0a293ca695d8f1f1f64b2889080
Affsyan <Affsyan@gmail.com>
2021-06-22 13:07:07 +0300

    Completed work les1

--- shopsite/shopsite/views.py
+++ shopsite/shopsite/views.py
@@ -1,3 +1,9 @@
+from django.shortcuts import render
+
 
 def index(request):
-    pass
+    return render(request, 'index.html')
+
+
+def contacts(request):
+    return render(request, 'contact.html')
