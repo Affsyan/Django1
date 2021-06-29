239da386054b85086db3232a15d6efffff012452
Affsyan <Affsyan@gmail.com>
2021-06-29 12:20:20 +0300

    Completed work les3

--- shopsite/mainapp/views.py
+++ shopsite/mainapp/views.py
@@ -1,17 +1,14 @@
 from django.shortcuts import render
+from mainapp.models import Product, ProductCategory
 
 
 def products(request):
     title = 'продукты'
-    links_menu = [
-        {'href': 'products_all', 'name': 'все'},
-        {'href': 'products_home', 'name': 'дом'},
-        {'href': 'products_office', 'name': 'офис'},
-        {'href': 'products_modern', 'name': 'модерн'},
-        {'href': 'products_classic', 'name': 'классика'},
-    ]
+    products = Product.objects.all()[:3]
+    links_menu = ProductCategory.objects.all().order_by('name')
     context = {
         'title': title,
         'links_menu': links_menu,
+        'products': products,
     }
     return render(request=request, template_name='mainapp/products.html', context=context)
--- shopsite/shopsite/views.py
+++ shopsite/shopsite/views.py
@@ -1,10 +1,14 @@
+
 from django.shortcuts import render
+from mainapp.models import Product
 
 
 def index(request):
     title = 'главная'
+    products = Product.objects.all()[:3]
     context = {
-        'title': title
+        'title': title,
+        'products': products,
     }
     return render(request=request, template_name='shopsite/index.html', context=context)
 
