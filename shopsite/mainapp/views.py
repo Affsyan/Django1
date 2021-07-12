b91b339ee38e72c6c5c6fd39988f8824d3df6eb9
Affsyan <Affsyan@gmail.com>
2021-07-12 17:03:34 +0300

    Completed les6

--- shopsite/mainapp/views.py
+++ shopsite/mainapp/views.py
@@ -1,37 +1,44 @@
-from basketapp.models import Basket
-from mainapp.models import Product, ProductCategory
+import random
+
 from django.shortcuts import render, get_object_or_404
 
+from basketapp.models import Basket
+from mainapp.models import ProductCategory, Product
 
-# def products(request):
-#     title = 'продукты'
-#     products = Product.objects.all()[:3]
-#     links_menu = ProductCategory.objects.all().order_by('name')
-#     context = {
-#         'title': title,
-#         'links_menu': links_menu,
-#         'products': products,
-#     }
-#     return render(request=request, template_name='mainapp/products.html', context=context)
 
+def get_basket(user):
+    if user.is_authenticated:
+        return Basket.objects.filter(user=user)
+    else:
+        return []
+
+
+def get_hot_product():
+    products = Product.objects.all()
+
+    return random.sample(list(products), 1)[0]
+
+
+def get_same_products(hot_product):
+    same_products = Product.objects.filter(category=hot_product.category).exclude(pk=hot_product.pk)
+
+    return same_products
 
 
 def products(request, pk=None):
     title = 'продукты/каталог'
-    basket = []
-    if request.user.is_authenticated:
-        basket = Basket.objects.filter(user=request.user)
+    basket = get_basket(request.user)
 
     links_menu = ProductCategory.objects.all()
-    products = Product.objects.all().order_by('name')[:6]
+    products = Product.objects.all().order_by('price')
 
     if pk is not None:
         if pk == 0:
-            products = Product.objects.all().order_by('name')[:6]
+            products = Product.objects.all().order_by('price')
             category = {'name': 'все'}
         else:
             category = get_object_or_404(ProductCategory, pk=pk)
-            products = Product.objects.filter(category__pk=pk).order_by('name')
+            products = Product.objects.filter(category__pk=pk).order_by('price')
 
         context = {
             'title': title,
@@ -41,13 +48,29 @@ def products(request, pk=None):
         }
         return render(request=request, template_name='mainapp/products.html', context=context)
 
-    same_products = Product.objects.all()
+    hot_product = get_hot_product()
+    same_products = get_same_products(hot_product)
 
     context = {
         'title': title,
         'links_menu': links_menu,
+        'hot_product': hot_product,
         'same_products': same_products,
         'products': products,
         'basket': basket,
     }
+
     return render(request=request, template_name='mainapp/products.html', context=context)
+
+
+def product(request, pk):
+    title = 'продукты'
+
+    context = {
+        'title': title,
+        'links_menu': ProductCategory.objects.all(),
+        'product': get_object_or_404(Product, pk=pk),
+        'basket': get_basket(request.user),
+    }
+
+    return render(request, 'mainapp/product.html', context)
