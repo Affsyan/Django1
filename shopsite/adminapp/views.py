e8009c0a71511cad59e4ef4fda3c09ad588e26bf
Affsyan <Affsyan@gmail.com>
2021-08-19 17:02:22 +0300

    les2.8

--- shopsite/adminapp/views.py
+++ shopsite/adminapp/views.py
@@ -1,15 +1,20 @@
+from django.db import connection
+from django.db.models import F
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 from django.http import HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 
-from adminapp.forms import ShopUserAdminEditForm, ProductEditForm
+from adminapp.forms import ShopUserAdminEditForm, ProductEditForm, ProductCategoryEditForm
 from authapp.forms import ShopUserRegisterForm
 from authapp.models import ShopUser
 from django.shortcuts import get_object_or_404, render
 from mainapp.models import Product, ProductCategory
 
 from django.views.generic.list import ListView
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 from django.contrib.auth.decorators import user_passes_test
+
 
 
 @user_passes_test(lambda u: u.is_superuser)
@@ -97,28 +102,6 @@ class UserCreateView(CreateView):
         return context
 
 
-def user_update(request, pk):
-    title = 'пользователи/рудактирование'
-
-    edit_user = get_object_or_404(ShopUser, pk=pk)
-
-    if request.method == 'POST':
-        edit_form = ShopUserAdminEditForm(request.POST, request.FILES, instance=edit_user)
-        if edit_form.is_valid():
-            edit_form.save()
-
-            return HttpResponseRedirect(reverse('admin_staff:users'))
-    else:
-        edit_form = ShopUserAdminEditForm(instance=edit_user)
-
-    context = {
-        'title': title,
-        'user_form': edit_form,
-    }
-
-    return render(request, 'adminapp/user_update.html', context)
-
-
 def user_delete(request, pk):
     title = 'пользователи/удаление'
 
@@ -169,8 +152,25 @@ def category_create(request):
     return render(request, 'adminapp/category_create.html', context)
 
 
-def category_update(request, pk):
-    pass
+class ProductCategoryUpdateView(UpdateView):
+    model = ProductCategory
+    template_name = 'adminapp/category_update.html'
+    success_url = reverse_lazy('admin_staff:categories')
+    form_class = ProductCategoryEditForm
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductCategoryUpdateView, self).get_context_data(**kwargs)
+        context['title'] = 'категории/редактирование'
+        return context
+
+    def form_valid(self, form):
+        if 'discount' in form.cleaned_data:
+            discount = form.cleaned_data['discount']
+            if discount:
+                self.object.product_set.update(price=F('price') * (1 - discount / 100))
+                db_profile_by_type(self.__class__, 'UPDATE', connection.queries)
+
+        return super().form_valid(form)
 
 
 def category_delete(request, pk):
@@ -279,3 +279,19 @@ def product_delete(request, pk):
     }
 
     return render(request, 'adminapp/product_delete.html', context)
+
+
+def db_profile_by_type(prefix, type, queries):
+    update_queries = list(filter(lambda x: type in x['sql'], queries))
+    print(f'db_profile {type} for {prefix}:')
+    [print(query['sql']) for query in update_queries]
+
+
+@receiver(pre_save, sender=ProductCategory)
+def product_is_active_productcategory_save(sender, instance, **kwargs):
+    if instance.pk:
+        instance.product_set.update(is_deleted=True)
+    else:
+        instance.product_set.update(is_deleted=False)
+
+    db_profile_by_type(sender, 'UPDATE', connection.queries)
